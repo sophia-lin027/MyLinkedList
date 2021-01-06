f6be5d3859a091e891c2c644988df52338cde4b6
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2021-01-06 17:41:56 -0500

    added instance variables and getData

--- Node.java
+++ Node.java
@@ -1,8 +1,13 @@
 public class Node {
+  private String data;
+  private Node next;
+  private Node prev;
+
   public Node(String value) {
     String data = value;
   }
 
-  private String data;
-  private Node next, prev;
+  private String getData() {
+    return data;
+  }
 }
