8c3953ad65e5e9748b4e36d36d9141a0333509c1
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2021-01-09 20:55:28 -0500

    added a new helper function that provides the node at a specific index and deleted an extra bracket in my toString method

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -13,15 +13,16 @@ public class MyLinkedList{
   }
 
   public boolean add(String value) {
-    Node new_value = new Node(value);
+    Node new_node = new Node(value);
     if (size == 0) {
-      this.start = new_value;
-      this.end = new_value;
+      this.start = new_node;
+      this.end = new_node;
     }
 
-    (this.end).setNext(new_value);
-    this.end = new_value;
     size++;
+    new_node.setPrev(this.end);
+    (this.end).setNext(new_node);
+    this.end = new_node;
     return true;
   }
 
@@ -30,7 +31,7 @@ public class MyLinkedList{
       throw new IndexOutOfBoundsException("The index " + index + " cannot be negative or greater than the size");
     }
 
-    Node current = start;
+    Node current = this.start;
     Node placeholder = new Node(value);
     if (current != null) {
       for (int i = 0; i < index && current.getNext() != null; i++) {
@@ -43,7 +44,7 @@ public class MyLinkedList{
   }
 
   public String get(int index) {
-    if (index < 0.0 || index > this.size()) {
+    if (index < 0.0 || index >= this.size()) {
       throw new IndexOutOfBoundsException("The index " + index + " cannot be negative or greater than the size");
     }
 
@@ -57,7 +58,7 @@ public class MyLinkedList{
   }
 
   public String set(int index, String value) {
-    if (index < 0.0 || index > size()) {
+    if (index < 0.0 || index >= size()) {
       throw new IndexOutOfBoundsException("The index " + index + " cannot be negative or greater than the size");
     }
 
@@ -71,18 +72,37 @@ public class MyLinkedList{
     return current.setData(value);
   }
 
+  private Node move(int index) {
+    if (index < 0.0 || index >= size()) {
+      throw new IndexOutOfBoundsException("The index " + index + " cannot be negative or greater than the size");
+    }
+
+    if (index == 0.0) {
+      return start;
+    }
+
+    if (index == size() - 1) {
+      return end;
+    }
+
+    Node current = start;
+    for (int i = 0; i < index; i++) {
+      current = current.getNext();
+    }
+    return current;
+  }
+
   public String toString() {
     String output = "";
-    if (start != null) {
-      Node current = start.getNext();
-      while (current != null) {
-        output = output + current.getData().toString();
-        if (current.getNext() != null) {
-          output = output + ", ";
-        }
-        current = current.getNext();
-      }
+    if(size()==0){
+      return "[]";
     }
+    Node current = start;
+    while(current.getNext() != null){
+      output += current.getData() + ", ";
+      current = current.getNext();
+    }
+    output += current.getData();
     return "[" + output + "]";
   }
 }
